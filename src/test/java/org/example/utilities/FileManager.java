364ce314caa914084a6ec9f4299525d0f009b987
efrainmavi <efrainmavi@gmail.com>
2025-06-07 09:58:02 -0600

    Adding logic to get page source.

--- src/test/java/org/example/utilities/FileManager.java
+++ src/test/java/org/example/utilities/FileManager.java
@@ -2,15 +2,18 @@ package org.example.utilities;
 
 import io.qameta.allure.Attachment;
 import org.apache.commons.io.FileUtils;
+import org.jsoup.Jsoup;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileManager {
 
     private static final String screenshotPath = "src/test/resources/screenshots";
+    private static final String pageSourcePath = "src/test/resources/pageStructure";
 
     public static void getScreenshot(String screenshotName){
         Logs.debug("Tomando Screenshot");
@@ -29,6 +32,8 @@ public class FileManager {
         try{
             Logs.info("Limpiando folder de evidencias");
             FileUtils.deleteDirectory(new File(screenshotPath));
+            FileUtils.deleteDirectory(new File(pageSourcePath));
+
         }catch (IOException ex){
             Logs.error("Error al Intentar limpiar la evidencia, %s: ", ex.getLocalizedMessage());
         }
@@ -37,5 +42,27 @@ public class FileManager {
     @Attachment(value = "screenshot", type = "image/png")
     public static byte[] getScreenshot(){
         return ((TakesScreenshot) new WebdriverProvider().get()).getScreenshotAs(OutputType.BYTES);
+    }
+
+    public static void getPageSource(String fileName){
+        Logs.info("Taking page source");
+
+        String path = String.format("%s/%s.html", pageSourcePath, fileName);
+
+        try{
+            File file = new File(path);
+
+            if (file.getParentFile().mkdir()){
+                FileWriter fileWriter = new FileWriter(file);
+                String pageSource = new WebdriverProvider().get().getPageSource();
+                if (pageSource != null) {
+                    fileWriter.write(Jsoup.parse(pageSource).toString());
+                }
+                fileWriter.close();
+            }
+
+        }catch (IOException ex){
+            Logs.error("Error taking page source: %s", ex.getLocalizedMessage());
+        }
     }
 }
