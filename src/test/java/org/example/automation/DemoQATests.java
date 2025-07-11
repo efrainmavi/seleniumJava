644edb959fde1fc93f87a83b2df709fa122eb82e
efrainmavi <efrainmavi@gmail.com>
2025-07-10 19:48:02 -0600

    add test acctions for keyboard and mouse

--- src/test/java/org/example/automation/DemoQATests.java
+++ src/test/java/org/example/automation/DemoQATests.java
@@ -3,7 +3,6 @@ package org.example.automation;
 import net.datafaker.Faker;
 import org.example.utilities.BaseTest;
 import org.example.utilities.Logs;
-import org.jspecify.annotations.Nullable;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
@@ -24,10 +23,14 @@ public class DemoQATests extends BaseTest {
 
         Logs.info("Full Name: %s ", fullName);
         WebElement userNameInput = driver.findElement(By.id("userName"));
-        userNameInput.click();
 
-        Logs.info("Aplicando acciones al teclado SHIFT y Escribiendo en mayusculas");
-        new Actions(driver).keyDown(Keys.SHIFT).sendKeys(fullName).keyUp(Keys.SHIFT).perform();
+        Logs.info("hacer click en el text box y luego presionar la tecla  SHIFT y Escribiendo en mayusculas");
+        new Actions(driver)
+                .click(userNameInput)
+                .keyDown(Keys.SHIFT)
+                .sendKeys(fullName)
+                .keyUp(Keys.SHIFT)
+                .perform();
 
         Logs.info("Verificando que el input este en mayúsculas");
         Assert.assertEquals(userNameInput.getAttribute("value"), fullName.toUpperCase());
@@ -44,32 +47,36 @@ public class DemoQATests extends BaseTest {
         Logs.info("current address: %s ", currentAddress);
         WebElement currentAddressInput = driver.findElement(By.id("currentAddress"));
         WebElement permanentAddressInput = driver.findElement(By.id("permanentAddress"));
+
+        //hacer scroll en la pagina para evitar anuncios
         ((JavascriptExecutor) driver).executeScript("window.scrollBy(0, 500);");
-        currentAddressInput.sendKeys(currentAddress);
-        currentAddressInput.click();
 
         String osSystem = System.getProperty("os.name").toLowerCase();
         if (osSystem.contains("win")){
-            Logs.info("Seleccionando y Copiando el contenido");
+
+            Logs.info("hacer click sobre el input, escribir el address y  selecciono y copio el contenido");
             new Actions(driver)
+                    .click(currentAddressInput)
+                    .sendKeys(currentAddress)
                     .keyDown(Keys.CONTROL)
                     .sendKeys("a")
                     .sendKeys("c")
                     .keyUp(Keys.CONTROL)
                     .perform();
 
-            Logs.info("Seleccionando y pegando el contenido");
-            permanentAddressInput.click();
-
+            //Pegando la direccion
             new Actions(driver)
+                    .click(permanentAddressInput)
                     .keyDown(Keys.CONTROL)
                     .sendKeys("v")
                     .keyUp(Keys.CONTROL)
                     .perform();
 
-        }else {
+        } else {
             Logs.info("Seleccionando y Copiando el contenido");
             new Actions(driver)
+                    .click(currentAddressInput)
+                    .sendKeys(currentAddress)
                     .keyDown(Keys.COMMAND)
                     .sendKeys("a")
                     .sendKeys("c")
@@ -79,9 +86,8 @@ public class DemoQATests extends BaseTest {
             Logs.info("Seleccionando y pegando el contenido");
             permanentAddressInput.isEnabled();
 
-            permanentAddressInput.click();
-
             new Actions(driver)
+                    .click(permanentAddressInput)
                     .keyDown(Keys.COMMAND)
                     .sendKeys("v")
                     .keyUp(Keys.COMMAND)
@@ -89,5 +95,38 @@ public class DemoQATests extends BaseTest {
         }
         Logs.info("Verificando que el input este en mayúsculas");
         Assert.assertEquals(currentAddressInput.getAttribute("value"), permanentAddressInput.getAttribute("value"));
+    }
+
+    @Test
+    public void testMouseAction(){
+        driver.get("https://demoqa.com/droppable");
+
+        WebElement figuraOrigen = driver.findElement(By.id("draggable"));
+        WebElement figuraDestino = driver.findElement(By.id("droppable"));
+
+        new Actions(driver)
+                .dragAndDrop(figuraOrigen, figuraDestino)
+                .perform();
+
+        Logs.info("Verificando que el label sea visible");
+        ((JavascriptExecutor) driver).executeScript("window.scrollBy(0, 300);");
+        Assert.assertTrue(driver.findElement(By.xpath("//p[text()='Dropped!']")).isDisplayed());
+    }
+
+    @Test
+    public void testTooltip() {
+        driver.get("https://demoqa.com/tool-tips");
+
+        WebElement btnVerde = driver.findElement(By.id("toolTipButton"));
+
+        Logs.info("Moviendo mouse al boton verde");
+        new Actions(driver)
+                .moveToElement(btnVerde)
+                .pause(2000)
+                .perform();
+
+        Logs.info("Verificando el tooltip");
+        Assert.assertEquals(btnVerde.getAttribute("aria-describedby"), "buttonToolTip");
+
     }
 }
